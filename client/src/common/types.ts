27ab4b7966eb7a15d29e6f2c180a5397d51304da
LeeHikari <liam.isle@gmail.com>
2022-02-26 16:34:41 +1100

    Setting up client-side environment

---
+++ client/src/common/types.ts
@@ -0,0 +1,9 @@
+export type LGA_Event = {
+    title: string | null;
+    description: string | null;
+    startDate: string;
+    endDate: string | null;
+    id: string;
+    eventImageUrl: string | null;
+    eventUrl: string | null;
+}
