6103f2805d4bb0a413d8e07996cd534edb370d54
BuildTools <unconfigured@null.spigotmc.org>
2022-03-10 18:58:17 +1100

    Nulls removed and dates changed to date type

--- client/src/common/types.ts
+++ client/src/common/types.ts
@@ -1,8 +1,8 @@
 export type LGAEvent = {
     title: string | null;
     description: string | null;
-    startDate: string;
-    endDate: string | null;
+    startDate: Date;
+    endDate: Date | null;
     id: string;
     eventImageUrl: string | null;
     eventUrl: string | null;
