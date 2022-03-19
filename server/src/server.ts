9ecdff459cb3814c2924f92b5cc11a888e7a699f
Jasper Dunn <jasperdunn93@gmail.com>
2022-03-19 15:20:54 +1000

    replace node server with playwright function

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,46 +1,21 @@
-import express from 'express'
-import cors from 'cors'
-import tcpPortUsed from 'tcp-port-used'
-import { prompt } from 'inquirer'
+import playwright from 'playwright'
 
-async function start(port = 5000): Promise<void> {
-  try {
-    const portIsUsed = await tcpPortUsed.check(port, 'localhost')
-
-    if (portIsUsed) {
-      const { shouldIncrementPort } = await prompt<{
-        shouldIncrementPort: boolean
-      }>([
-        {
-          type: 'confirm',
-          name: 'shouldIncrementPort',
-          message: `Port ${port} is already in use.\nWould you like to try port ${
-            port + 1
-          }?`,
-        },
-      ])
-
-      if (shouldIncrementPort) {
-        start(++port)
-      } else {
-        process.exit()
-      }
-    } else {
-      const server = express()
-
-      server.use(cors({ origin: 'http://localhost:3000' }))
-
-      server.get('/hello-world', (request, response) => {
-        response.send({ message: 'hello world' })
-      })
-
-      server.listen(port, () => {
-        console.log(`Server is listening at http://localhost:${port}`)
-      })
-    }
-  } catch (error) {
-    console.error(error)
-  }
+async function start(): Promise<void> {
+  const browser = await playwright.webkit.launch()
+  const page = await browser.newPage()
+  await page.goto('https://books.toscrape.com/')
+  const books = await page.$$eval('.product_pod', (elements) => {
+    const data: unknown[] = []
+    elements.forEach((book) => {
+      const name = book.querySelector('h3').innerText
+      const price = book.querySelector('.price_color').innerText
+      const stock = book.querySelector('.availability').innerText
+      data.push({ name, price, stock })
+    })
+    return data
+  })
+  console.log(books)
+  await browser.close()
 }
 
 start()
