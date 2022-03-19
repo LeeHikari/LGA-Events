c34fd1b31c7df78703b8d2ecb51657233c021d39
Jasper Dunn <jasperdunn93@gmail.com>
2022-03-19 15:48:22 +1000

    renaming project

--- scraper/src/scraper.ts
+++ scraper/src/scraper.ts
@@ -1,18 +1,18 @@
 import playwright from 'playwright'
 
+type Book = { name: string; price: string; stock: string }
+
 async function start(): Promise<void> {
   const browser = await playwright.webkit.launch()
   const page = await browser.newPage()
   await page.goto('https://books.toscrape.com/')
-  const books = await page.$$eval('.product_pod', (elements) => {
-    const data: unknown[] = []
-    elements.forEach((book) => {
+  const books: Book[] = await page.$$eval('.product_pod', (elements) => {
+    return elements.map((book) => {
       const name = book.querySelector('h3').innerText
       const price = book.querySelector('.price_color').innerText
       const stock = book.querySelector('.availability').innerText
-      data.push({ name, price, stock })
+      return { name, price, stock }
     })
-    return data
   })
   console.log(books)
   await browser.close()
---
+++ server/src/scraper.ts
@@ -0,0 +1,21 @@
+import playwright from 'playwright';
+
+type Book = { name: string; price: string; stock: string };
+
+async function start(): Promise<void> {
+  const browser = await playwright.webkit.launch();
+  const page = await browser.newPage();
+  await page.goto('https://books.toscrape.com/');
+  const books: Book[] = await page.$$eval('.product_pod', (elements) => {
+    return elements.map((book) => {
+      const name = book.querySelector('h3').innerText;
+      const price = book.querySelector('.price_color').innerText;
+      const stock = book.querySelector('.availability').innerText;
+      return { name, price, stock };
+    });
+  });
+  console.log(books);
+  await browser.close();
+}
+
+start();
