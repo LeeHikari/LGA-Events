4785e58ca0ef65dee251a389fb7a5d0575ce50c8
Jasper Dunn <jasperdunn93@gmail.com>
2022-03-20 21:29:15 +1000

    fix up scraper project
    Co-authored-by: Liam Isle <LeeHikari@users.noreply.github.com>

--- scraper/src/LGAs/parramatta.ts
+++ scraper/src/LGAs/parramatta.ts
@@ -4,35 +4,95 @@ import playwright from 'playwright'
 export async function ScrapeParramattaElements(
   browser: playwright.Browser
 ): Promise<void> {
-  const page = await browser.newPage()
-  await page.goto('https://atparramatta.com/whats-on')
-  await browser.close()
-  const events: LGAEvent[] = await page.$$eval(
-    '.col-wrap',
-    (elements: HTMLElement[]) => {
-      return elements.map((event): LGAEvent => {
-        const dates = page.locator('div:event-dates').textContent.toString()
-        return {
-          title: page.locator('div:title').textContent.toString() || '',
-          description: event.querySelector('div')?.innerText || null,
-          startDate:
-            new Date(
-              Date.parse(dates.substring(0, dates.indexOf('-') - 1) || '')
-            ) || new Date(),
-          endDate:
-            new Date(
-              Date.parse(dates.substring(dates.indexOf('-') + 1) || '')
-            ) || new Date(),
-          id:
-            `${dates.substring(0, dates.indexOf('-'))}-${page
-              .locator('div:title')
-              .textContent.toString()
-              .replace(' ', '-')}` || '',
-          eventImageUrl: event.querySelector('div')?.innerText || '',
-          eventUrl: event.querySelector('div')?.innerText || '',
+  try {
+    const page = await browser.newPage()
+
+    // Only used for dev logging purposes
+    // page.$$eval executes code on a headless browser,
+    // so console.logs inside this function will not appear on the node console.
+    page.on('console', (msg) => {
+      const args = msg.args()
+      for (let i = 0; i < args.length; ++i) {
+        console.log(`${i}: ${args[i]}`)
+      }
+    })
+
+    await page.goto('https://atparramatta.com/whats-on')
+    const events: LGAEvent[] = await page.$$eval(
+      'a.col-wrap',
+      (elements: HTMLElement[]) => {
+        function getId(dateString: string, title: string): string {
+          return `${dateString.slice(
+            0,
+            dateString.indexOf('-')
+          )}-${title.replace(' ', '-')}`
         }
-      })
-    }
-  )
-  console.log(events)
+
+        // replace title with eventUrl when you have it working
+        function getDateParts(
+          dateString: string,
+          title: string
+        ): {
+          startDate: Date
+          endDate: Date | null
+        } {
+          const dateStringParts = dateString.split(' - ')
+
+          if (dateStringParts.length === 0 || dateStringParts.length > 2) {
+            throw new Error(
+              `Date format has changed on https://atparramatta.com/whats-on Received ${JSON.stringify(
+                { dateStringParts, title }
+              )}`
+            )
+          }
+
+          const startDate = new Date(dateStringParts[0])
+          const endDate = dateStringParts[1]
+            ? new Date(dateStringParts[1])
+            : null
+
+          return { startDate, endDate }
+        }
+
+        return elements
+          .map((element): LGAEvent | null => {
+            const dateString =
+              element.querySelector(
+                'div.content-block div.content-details div.event-date'
+              )?.textContent || null
+
+            if (!dateString) {
+              return null
+            }
+
+            const title =
+              element.querySelector('div.content-block h4.title')
+                ?.textContent || null
+
+            if (!title) {
+              return null
+            }
+
+            const { startDate, endDate } = getDateParts(dateString, title)
+
+            return {
+              title,
+              description:
+                element.querySelector(
+                  'div.content-block div.content-details div.description'
+                )?.textContent || null,
+              startDate,
+              endDate,
+              id: getId(dateString, title),
+              eventImageUrl: element.querySelector('div')?.innerText || '',
+              eventUrl: element.querySelector('div')?.innerText || '',
+            }
+          })
+          .filter((event): event is LGAEvent => event !== null)
+      }
+    )
+    console.log(events)
+  } catch (error) {
+    console.error('\x1b[41m%s\x1b[0m', `Parramatta - ${error}`)
+  }
 }
--- scraper/src/scraper.ts
+++ scraper/src/scraper.ts
@@ -2,9 +2,12 @@ import playwright from 'playwright'
 import { ScrapeParramattaElements } from './LGAs/parramatta'
 
 export async function start(): Promise<void> {
+  console.log('\x1b[44m%s\x1b[0m', 'launch')
   const browser = await playwright.webkit.launch()
-  ScrapeParramattaElements(browser);
+  console.log('\x1b[44m%s\x1b[0m', 'launched')
+  await ScrapeParramattaElements(browser)
   await browser.close()
+  console.log('\x1b[44m%s\x1b[0m', 'closed')
 }
 
 start()
