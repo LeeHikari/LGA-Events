c29933958f65f683063ce91ab3aee1f47db01186
Necropixel15616 <jacob.tuckwell@outlook.com>
2022-03-20 15:19:42 +1100

    Cleaned up parramatta.ts

--- scraper/src/LGAs/parramatta.ts
+++ scraper/src/LGAs/parramatta.ts
@@ -1,30 +1,31 @@
 import { LGAEvent } from '../types'
 import playwright from 'playwright'
 
-export async function ScrapeParramattaElements(browser: playwright.Browser){
-    const page = await browser.newPage()
-    await page.goto('https://atparramatta.com/whats-on')
-    await browser.close()
-    const events: LGAEvent[] = await page.$$eval('.product_pod', (elements: any[]) => {
-        return elements.map((event) => {
-            const title = event.querySelector('').innerText
-            const description = event.querySelector('').innerText
-            const startDate = event.querySelector('').innerText
-            const endDate = event.querySelector('').innerText
-            const id = event.querySelector('').innerText
-            const eventImageUrl = event.querySelector('').innerText
-            const eventUrl = event.querySelector('').innerText
-            return { 
-                title,
-                description,
-                startDate,
-                endDate,
-                id,
-                eventImageUrl,
-                eventUrl
-            }
-        })
-    })
-
-    console.log(events)
+export async function ScrapeParramattaElements(
+  browser: playwright.Browser
+): Promise<void> {
+  const page = await browser.newPage()
+  await page.goto('https://atparramatta.com/whats-on')
+  await browser.close()
+  const events: LGAEvent[] = await page.$$eval(
+    '.product_pod',
+    (elements: HTMLElement[]) => {
+      return elements.map((event): LGAEvent => {
+        return {
+          title: event.querySelector('div')?.innerText || '',
+          description: event.querySelector('div')?.innerText || null,
+          startDate:
+            new Date(Date.parse(event.querySelector('div')?.innerText || '')) ||
+            new Date(),
+          endDate:
+            new Date(Date.parse(event.querySelector('div')?.innerText || '')) ||
+            new Date(),
+          id: event.querySelector('div')?.innerText || '',
+          eventImageUrl: event.querySelector('div')?.innerText || '',
+          eventUrl: event.querySelector('div')?.innerText || '',
+        }
+      })
+    }
+  )
+  console.log(events)
 }
