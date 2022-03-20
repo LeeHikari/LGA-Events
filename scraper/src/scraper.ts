6b63a5f63c2a1d4dc57d8ff8b58b686357c7a225
Liam <liam.isle@gmail.com>
2022-03-20 11:11:53 +1100

    Moved code to parramatta.ts

--- scraper/src/scraper.ts
+++ scraper/src/scraper.ts
@@ -1,20 +1,9 @@
 import playwright from 'playwright'
+import { ScrapeParramattaElements } from './LGAs/parramatta'
 
-type Book = { name: string; price: string; stock: string }
-
-async function start(): Promise<void> {
+export async function start(): Promise<void> {
   const browser = await playwright.webkit.launch()
-  const page = await browser.newPage()
-  await page.goto('https://books.toscrape.com/')
-  const books: Book[] = await page.$$eval('.product_pod', (elements) => {
-    return elements.map((book) => {
-      const name = book.querySelector('h3').innerText
-      const price = book.querySelector('.price_color').innerText
-      const stock = book.querySelector('.availability').innerText
-      return { name, price, stock }
-    })
-  })
-  console.log(books)
+  ScrapeParramattaElements(browser);
   await browser.close()
 }
 
