1294534213d032e56d6d4daf1f2dad3d0d6772c0
Necropixel15616 <jacob.tuckwell@outlook.com>
2022-03-25 15:02:32 +1100

    Changed File Directory of LGAEvents.json to src folder

--- scraper/src/common/utils.ts
+++ scraper/src/common/utils.ts
@@ -3,5 +3,8 @@ import { appendFile } from 'fs/promises'
 
 export async function ExportToJson(events: LGAEvent[]): Promise<void> {
   const jsonLGAEventString = JSON.stringify(events)
-  await appendFile(`${__filename}/LGAEvents.json`, jsonLGAEventString)
+  await appendFile(
+    `${__filename}\\..\\..\\..\\src\\LGAEvents.json`,
+    jsonLGAEventString
+  )
 }
