e441d79b825bfe7cd91c2245e9e1ee89d22721aa
Liam <liam.isle@gmail.com>
2022-03-31 17:11:47 +1100

    Ora and Chalk added to utils.ts

--- scraper/src/common/utils.ts
+++ scraper/src/common/utils.ts
@@ -1,15 +1,21 @@
 import { LGAEvent } from './types'
 import { writeFile } from 'fs/promises'
 import fs from 'fs'
+import ora from 'ora'
+import chalk from 'chalk'
 
 export async function ExportToJson(events: LGAEvent[]): Promise<void> {
+  const exportingJson = ora('Exporting to JSON').start()
+
   try {
     if (!fs.existsSync('out')) {
       fs.mkdirSync('out')
     }
 
     await writeFile('out/LGAEvents.json', JSON.stringify(events))
+
+    exportingJson.succeed(chalk.blue(`Exporting to JSON successful`))
   } catch (error) {
-    console.error(error)
+    exportingJson.fail(chalk.red(`Exporting to JSON failed - ${error}`))
   }
 }
