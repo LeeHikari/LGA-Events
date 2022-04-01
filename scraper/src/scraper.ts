a0a3679d4b0bb00d6dd48b6170af36bcca56d9bd
Liam <liam.isle@gmail.com>
2022-04-01 21:07:18 +1100

    Logging and error handling and --dev & --log arguments added

--- scraper/src/scraper.ts
+++ scraper/src/scraper.ts
@@ -1,27 +1,40 @@
 import { webkit } from 'playwright'
 import { LGAEvent } from './common/types'
-import { ExportToJson } from './common/utils'
+import { exportToJson } from './common/utils'
 import { scrapeParramatta } from './LGAs/parramatta'
 import ora from 'ora'
+import 'dotenv/config'
 import chalk from 'chalk'
+
+//Used to see if dotenv package is working?
+//console.log(process.env)
 
 export async function start(): Promise<void> {
   const launchingBrowser = ora('Launching browser').start()
-  const browser = await webkit.launch()
-  const context = await browser.newContext()
-  launchingBrowser.succeed(chalk.yellow.inverse('Browser launched successfully'))
 
-  const events: LGAEvent[] = await scrapeParramatta(await context.newPage())
+  try {
+    const browser = await webkit.launch()
+    const context = await browser.newContext()
 
-  const mode = process.argv[2]
-  if (mode === '--dev' ){
-    await ExportToJson(events)
-  } else if(mode === '--log'){
-    console.log(events)
+    launchingBrowser.succeed(
+      chalk.yellow.inverse('Browser launched successfully')
+    )
+
+    const events: LGAEvent[] = await scrapeParramatta(await context.newPage())
+
+    const mode = process.argv[2]
+    if (mode === '--dev') {
+      await exportToJson(events)
+    } else if (mode === '--log') {
+      console.log(events)
+    }
+    
+    await browser.close()
+    launchingBrowser.succeed(chalk.yellow.inverse('Scraping finished'))
+    
+  } catch (error) {
+    launchingBrowser.fail(chalk.red(`Error occured in Start() - ${error}`))
   }
-  
-  await browser.close()
-  launchingBrowser.succeed(chalk.yellow.inverse('Scraping finished'))
 }
 
 start()
