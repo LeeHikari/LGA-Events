7919bb8c418962161323da2922e09044bcade65c
Liam <liam.isle@gmail.com>
2022-04-01 21:01:39 +1100

    Function for uploading to Google Cloud Storage added

--- scraper/src/common/utils.ts
+++ scraper/src/common/utils.ts
@@ -1,10 +1,11 @@
 import { LGAEvent } from './types'
 import { writeFile } from 'fs/promises'
+import { Storage } from '@google-cloud/storage'
 import fs from 'fs'
 import ora from 'ora'
 import chalk from 'chalk'
 
-export async function ExportToJson(events: LGAEvent[]): Promise<void> {
+export async function exportToJson(events: LGAEvent[]): Promise<void> {
   const exportingJson = ora('Exporting to JSON').start()
 
   try {
@@ -18,5 +19,21 @@ export async function ExportToJson(events: LGAEvent[]): Promise<void> {
     exportingJson.succeed(chalk.blue(`Export to JSON successful`))
   } catch (error) {
     exportingJson.fail(chalk.red(`Export to JSON failed - ${error}`))
+  }
+
+  uploadToCloud()
+}
+
+export async function uploadToCloud(): Promise<void> {
+  const uploadingToCloud = ora('Uploading to cloud').start()
+  
+  try {
+    const storage = new Storage()
+    await storage.bucket('lgaevents').upload('./out/LGAEvents.json')
+
+    uploadingToCloud.succeed(chalk.blue(`Uploaded to cloud successfully`))
+    
+  } catch (error) {
+    uploadingToCloud.fail(chalk.red(`Uploaded to cloud failed - ${error}`))
   }
 }
