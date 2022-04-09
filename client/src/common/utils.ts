7b4b02142134d63433ea71ba96427d127edb7140
Liam <liam.isle@gmail.com>
2022-04-09 16:46:53 +1000

    Removed hour from date

--- client/src/common/utils.ts
+++ client/src/common/utils.ts
@@ -1,5 +1,5 @@
 import { format } from 'date-fns'
 
-export function formatDate(date:Date): string {
-   return format(new Date(date), 'dd/MM/yyyy HH:mm')
+export function formatDate(date: Date): string {
+  return format(new Date(date), 'dd/MM/yyyy ')
 }
